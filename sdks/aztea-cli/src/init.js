f170d6875b508d53484455c5922f3347f776c6d5
anay <agarodia98@gmail.com>
2026-04-29 16:46:37 -0400

    Bump aztea-cli for frontend brand release

--- sdks/aztea-cli/src/init.js
+++ sdks/aztea-cli/src/init.js
@@ -28,7 +28,7 @@ function post(url, body) {
       headers: {
         'Content-Type': 'application/json',
         'Content-Length': Buffer.byteLength(payload),
-        'User-Agent': 'aztea-cli/0.14.0',
+        'User-Agent': 'aztea-cli/0.14.1',
       },
     }, (res) => {
       let data = ''
